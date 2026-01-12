018b9efcb9b22b4c3ea4be490de01c83e1c8b67a
kkxiaxia <157866633+kkxiaxia@users.noreply.github.com>
2026-01-12 14:06:20 +0800

    Create config_diff.tcl

---
+++ config_diff.tcl
@@ -0,0 +1,201 @@
+dict set ::compare::diffPorc E0248 [dict create \
+    miss_diff_map [list Diff_003] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0308 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+    # miss_diff_map [list Diff_006]
+dict set ::compare::diffPorc E0200 [dict create \
+    miss_diff_map [list Diff_029] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0228 [dict create \
+    miss_diff_map [list Diff_041_1 Diff_309 Diff_ca_033] \
+    false_diff_map [list Diff_041_2 Diff_ca_012] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0259 [dict create \
+    miss_diff_map [list Diff_102 Diff_100] \
+    false_diff_map [list Diff_052] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0236 [dict create \
+    miss_diff_map [list Diff_318 Diff_ca_034] \
+    false_diff_map [list Diff_319 Diff_ca_008] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0260 [dict create \
+    miss_diff_map [list Diff_366-2 Diff_365] \
+    false_diff_map [list Diff_095] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0251 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_ca_009 Diff_019] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0233 [dict create \
+    miss_diff_map [list Diff_ca_014 Diff_ca_check_s_not_E0233] \
+    false_diff_map [list Diff_049 Diff_ca_E0233_is_UDP Diff_ca_check_E_E0233] \
+    special_diff [list Diff_ca_000]] 
+    # special_diff [list Diff_ca_015]]
+dict set ::compare::diffPorc E0285 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_327] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0344 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_370] \
+    special_diff [list ]]
+# special_diff [list Diff_109]]
+dict set ::compare::diffPorc E0183 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_163 Diff_161]]
+dict set ::compare::diffPorc E0077 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_009]]
+dict set ::compare::diffPorc E0254 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_045]]
+dict set ::compare::diffPorc E0145 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_076]]
+dict set ::compare::diffPorc E0232 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_316]]
+dict set ::compare::diffPorc E0035 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_439]]
+dict set ::compare::diffPorc E0253 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_537]]
+dict set ::compare::diffPorc E0297 [dict create \
+    miss_diff_map [list Diff_ca_032] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_627]]
+dict set ::compare::diffPorc E0291 [dict create \
+    miss_diff_map [list Diff_ca_032] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_627]]
+dict set ::compare::diffPorc E0281 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_ca_035] \
+    special_diff [list ]]
+# special_diff [list Diff_627]]
+dict set ::compare::diffPorc E0292 [dict create \
+    miss_diff_map  [list Diff_ca_032] \
+    false_diff_map  [list ] \
+    special_diff  [list ]] 
+    # special_diff  [list Diff_627]]
+dict set ::compare::diffPorc E0338 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_ca_007] \
+    special_diff [list ]]
+# special_diff [list Diff_594]]
+dict set ::compare::diffPorc E0177 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_502]]
+dict set ::compare::diffPorc E0154 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_387 Diff_594]]
+dict set ::compare::diffPorc E0125 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_482]]
+dict set ::compare::diffPorc E0267 [dict create \
+    miss_diff_map [list Diff_ca_032] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+    # special_diff [list Diff_594]]
+    # 实际为Diff_400, 只是场景和Diff_594一致
+dict set ::compare::diffPorc E0239 [dict create \
+    miss_diff_map [list Diff_ca_023] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_264]]
+dict set ::compare::diffPorc E0089 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_ca_024] \
+    special_diff [list ]]
+# special_diff [list Diff_652]]
+dict set ::compare::diffPorc E0313 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+# special_diff [list Diff_289]]
+dict set ::compare::diffPorc E0220 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list ] \
+    special_diff [list redo_objunmatch]]
+# special_diff [list Diff_475]]
+dict set ::compare::diffPorc E0364 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_ca_001] \
+    special_diff [list ]]
+# 2025-0917: 恢复之前删除的E0257和E0289的差异proc(当前已注释)
+# dict set ::compare::diffPorc E0257 [dict create \
+#     miss_diff_map [list Diff_032] \
+#     false_diff_map [list Diff_030] \
+#     special_diff [list ]]
+# dict set ::compare::diffPorc E0289 [dict create \
+#     miss_diff_map [list ] \
+#     false_diff_map [list ] \
+#     special_diff [list Diff_047]]
+dict set ::compare::diffPorc E0034 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_ca_003] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0255 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_ca_013] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0261 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_ca_022] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0243 [dict create \
+    miss_diff_map [list Diff_ca_031 Diff_ca_030] \
+    false_diff_map [list Diff_ca_017] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0246 [dict create \
+    miss_diff_map [list Diff_ca_021] \
+    false_diff_map [list Diff_ca_036] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0027 [dict create \
+    miss_diff_map [list Diff_ca_019] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0026 [dict create \
+    miss_diff_map [list Diff_ca_020] \
+    false_diff_map [list ] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0332 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_ca_018] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0092 [dict create \
+    miss_diff_map [list ] \
+    false_diff_map [list Diff_ca_025] \
+    special_diff [list ]]
+dict set ::compare::diffPorc E0250 [dict create \
+    miss_diff_map [list Diff_ca_029] \
+    false_diff_map [list Diff_ca_026] \
+    special_diff [list ]]
